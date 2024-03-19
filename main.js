e10a17c33819422608d9794ec3b6f860f22260f5
devitoangelo <angelodevito4@gmail.com>
2024-03-19 15:45:27 +0100

    push in new array email and die email in the list

--- main.js
+++ main.js
@@ -5,23 +5,38 @@ console.log("ciao");
 const { createApp } = Vue
 
 createApp({
-  data() {
-    return {
-        randomEmail: 0
-    
+    data() {
+        return {
+            randomEmail: []    //creare un array vuoto dove mettere le 10 email creare 
 
+
+        }
+    },
+    methods: {
+        callApi() {
+
+            for (let i = 0; i < 10; i++) {         //creare un ciclo che crea 10 email 
+                const element = this.randomEmail[i];
+                console.log(element);
+                axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
+                    .then((response) => {
+
+
+                        this.randomEmail.push(response.data.response)// prendo .data e dentro .response che sarebbe l'email e la push nel nuovo array
+
+                        console.log(this.randomEmail);
+                    })
+
+            }
+
+        }
+    },
+    mounted() {
+        this.callApi()   //richiamo la funzione in mounted
     }
-  },
-  mounted() {
-
-    axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
-    .then((response)=> {
-
-        console.log(response);
-
-        this.randomEmail = response.data.response
-    })
-  }
 
 
 }).mount('#app')
+
+
+
