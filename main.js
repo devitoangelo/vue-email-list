21dea3cda5b95f9d22108755ecf7fc3dfce250d7
devitoangelo <angelodevito4@gmail.com>
2024-03-19 15:07:06 +0100

    generate mounted axios

--- main.js
+++ main.js
@@ -1,1 +1,27 @@
 console.log("ciao");
+
+// https://flynn.boolean.careers/exercises/api/random/mail
+
+const { createApp } = Vue
+
+createApp({
+  data() {
+    return {
+        randomEmail: 0
+    
+
+    }
+  },
+  mounted() {
+
+    axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
+    .then((response)=> {
+
+        console.log(response);
+
+        this.randomEmail = response.data.response
+    })
+  }
+
+
+}).mount('#app')
